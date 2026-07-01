5f5132b4c789176bb5ba05fdbe55f1f1c37b8fe2
imMatheus <matheusmendesbarata03@gmail.com>
2026-06-30 22:55:48 -0700

    changed the dock image

--- src/main/index.ts
+++ src/main/index.ts
@@ -50,6 +50,13 @@ app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
+  // On macOS the dock icon isn't taken from the BrowserWindow `icon` option.
+  // Set it explicitly so dev/preview runs show the app icon instead of the
+  // default Electron icon. (Packaged builds use build/icon.icns.)
+  if (process.platform === 'darwin' && app.dock) {
+    app.dock.setIcon(icon)
+  }
+
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
